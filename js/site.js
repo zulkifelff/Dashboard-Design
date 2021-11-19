7b77d90126079f1e1dcee005d311541bac8e6373
Muhammad Zulkifel <zulkifel@gmail.com>
2021-11-19 18:16:20 +0500

    Modal Creation

--- js/site.js
+++ js/site.js
@@ -1,7 +1,18 @@
+
+
 jQuery(function () {
     jQuery("#datepicker").datepicker({
         showButtonPanel: false
     });
+
+    jQuery('#datepicker-createload').datepicker({});
+
+    jQuery(document).on("click", ".button-styling-continue", function (e)
+    {
+        jQuery('.load-input-section-1').fadeOut('fast','linear',eventCallBack(e));
+       jQuery('.load-input-section-2').fadeIn('slow','linear',eventCallBack(e));
+    });
+
 });
 
 function SearchBarBlurred() {
@@ -10,4 +21,8 @@ function SearchBarBlurred() {
 
 function SearchBarFocused() {
     $('.search-result').addClass('opened');
+}
+
+function eventCallBack(e) {
+console.log(e);
 }
